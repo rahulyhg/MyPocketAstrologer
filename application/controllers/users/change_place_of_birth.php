b6ece1fc31c7201163d6e57615e4d5d8b01cddb3
Bandana <bandanarocks@gmail.com>
2015-08-24 20:02:15 +0545

    more apis on profile edit

---
+++ application/controllers/users/change_place_of_birth.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require APPPATH.'/libraries/REST_Controller.php';
+
+class Change_place_of_birth extends REST_Controller {
+
+	public function index_get() {
+
+	}
+
+	public function index_post() {
+
+		$params = json_decode(file_get_contents('php://input'),true);
+
+		try {
+
+			if(!$this->input->server('PHP_AUTH_USER') || !$this->input->server('PHP_AUTH_PW')) {
+
+	        	throw new Exception("Access Forbidden!!");
+	        }
+
+	        $api = ApiAuthentication::find_by_user_and_authentication_key($this->input->server('PHP_AUTH_USER'), $this->input->server('PHP_AUTH_PW'));
+	        
+	        if(!$api) 
+	        	throw new Exception("Access Forbidden");
+			
+			$user = User::find_valid_by_id($params['current_user_id']);
+
+			if(!$user)
+				throw new Exception("Invalid User Request");
+
+			$user->place_of_birth = $params['place_of_birth'];
+
+			$user->save();
+
+			$response = $this->response(array(
+							'status'	=>	'SUCCESS',
+							'message'=>'Place of Birth Edited Successfully',
+							'user'=> $user->first_name,
+							'data' => null
+							));
+			
+			$this->response($response);
+		}
+
+		catch(Exception $e) {
+
+			$response = json_encode(array(
+							'status' =>	'ERROR',
+							'message' => $e->getMessage(),
+							));
+			
+			header('HTTP/1.1 400 Bad Request', true, 400);
+
+			echo $response;
+		}
+	}
+}
